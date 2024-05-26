c922319d754bc8aed67f3ab04aaec55d76044004
BK <3031011@naver.com>
2024-05-26 18:19:08 +0900

    a

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,57 @@
+'use client';
+
+import { useEffect, useState } from 'react';
+import socket from '../socket';
+
 export default function Home() {
-  return 'home';
+  const [isConnected, setIsConnected] = useState(false);
+  const [transport, setTransport] = useState('N/A');
+  const [data, setData] = useState('');
+
+  useEffect(() => {
+    function onConnect() {
+      setIsConnected(true);
+      setTransport(socket.io.engine.transport.name);
+
+      socket.io.engine.on('upgrade', (transport) => {
+        setTransport(transport.name);
+      });
+    }
+
+    if (socket.connected) {
+      onConnect();
+    }
+
+    function onDisconnect() {
+      setIsConnected(false);
+      setTransport('N/A');
+    }
+
+    socket.on('connect', onConnect);
+    socket.on('disconnect', onDisconnect);
+    socket.on('res', (data) => {
+      setData(data);
+    });
+
+    return () => {
+      socket.off('connect', onConnect);
+      socket.off('disconnect', onDisconnect);
+    };
+  }, [socket]);
+
+  return (
+    <div>
+      <p>Status: {isConnected ? 'connected' : 'disconnected'}</p>
+      <p>Transport: {transport}</p>
+      <button
+        type="button"
+        onClick={() => {
+          socket.emit('abc', 'world');
+        }}
+      >
+        emit
+      </button>
+      {data}
+    </div>
+  );
 }
