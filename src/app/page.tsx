62c61a1d8e933417163888bbfc2c503e8e046d97
jypark38 <126536351+jypark38@users.noreply.github.com>
2024-05-26 18:52:16 +0900

    프로젝트 초기 설정 (#1)
    * d
    
    * a
    
    * a
    
    * jest 추가
    
    * b
    
    * b
    
    * a
    
    * a
    
    * a
    
    * add ignore
    
    * d
    
    ---------
    
    Co-authored-by: BK <3031011@naver.com>

---
+++ src/app/page.tsx
@@ -0,0 +1,57 @@
+'use client';
+
+import { useEffect, useState } from 'react';
+import socket from '../socket';
+
+export default function Home() {
+  const [isConnected, setIsConnected] = useState(false);
+  const [transport, setTransport] = useState('N/A');
+  const [data, setData] = useState('');
+
+  useEffect(() => {
+    function onConnect() {
+      setIsConnected(true);
+      setTransport(socket.io.engine.transport.name);
+
+      socket.io.engine.on('upgrade', (trans) => {
+        setTransport(trans.name);
+      });
+    }
+
+    if (socket.connected) {
+      onConnect();
+    }
+
+    function onDisconnect() {
+      setIsConnected(false);
+      setTransport('N/A');
+    }
+
+    socket.on('connect', onConnect);
+    socket.on('disconnect', onDisconnect);
+    socket.on('res', (resData) => {
+      setData(resData);
+    });
+
+    return () => {
+      socket.off('connect', onConnect);
+      socket.off('disconnect', onDisconnect);
+    };
+  }, []);
+
+  return (
+    <div>
+      <p>Status: {isConnected ? 'connected' : 'disconnected'}</p>
+      <p>Transport: {transport}</p>
+      <button
+        type="button"
+        onClick={() => {
+          socket.emit('abc', 'world');
+        }}
+      >
+        emit
+      </button>
+      {data}
+    </div>
+  );
+}
