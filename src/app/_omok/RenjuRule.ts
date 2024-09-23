5c156159b4689d8948c3c342e26e73ec72d87da5
BK <3031011@naver.com>
2024-09-24 00:14:27 +0900

    refactor(RenjuRule): 3*3 금수 알고리즘 중복된 코드 구조 개선

--- src/app/_omok/RenjuRule.ts
+++ src/app/_omok/RenjuRule.ts
@@ -63,107 +63,50 @@ class RenjuRule {
 
   private samsam() {
     const { x, y } = this.position;
-    // 3*3 금수가 가능한 위치, V: 가능 위치 O: 흑돌 X: 공백
-    // 가능한 케이스, 간접 혹은 직접 막혀있지 않아야 함
-    // 1. 기본 VVOOVV
-    // 2. 띈 경우 XVOXOVX
-    // 3. 낀 경우 XOVVOX
 
     /** open two인지 체크 */
     const checkOpenTwo = (position1: Position, position2: Position) => {
       const { dx, dy } = Board.getDirection(position1, position2);
       const distance = getDistance(position1, position2);
+
       const bothSideEmpty =
         this.board.canDropStone(createPosition(position1.x + -dx, position1.y + -dy)) &&
         this.board.canDropStone(createPosition(position2.x + dx, position2.y + dy));
 
-      switch (distance) {
-        case 1:
-          return bothSideEmpty;
+      if (bothSideEmpty) {
+        if (distance === 1) return true;
 
-        case 2: // 양 사이드, 두 돌 사이가 빈 셀인지
+        const innerFirstPosition = createPosition(position1.x + dx, position1.y + dy);
+
+        if (distance === 2) return this.board.canDropStone(innerFirstPosition);
+
+        const innerSecondPosition = createPosition(position1.x + dx * 2, position1.y + dy * 2);
+
+        if (distance === 3) {
           return (
-            bothSideEmpty &&
-            this.board.canDropStone(createPosition(position1.x + dx, position1.y + dy))
+            this.board.canDropStone(innerFirstPosition) &&
+            this.board.canDropStone(innerSecondPosition)
           );
-
-        case 3: // 양 사이드, 두 돌 사이가 빈 셀인지
-          return (
-            bothSideEmpty &&
-            this.board.canDropStone(createPosition(position1.x + dx, position1.y + dy)) &&
-            this.board.canDropStone(createPosition(position1.x + dx * 2, position1.y + dy * 2))
-          );
-
-        default:
-          return false;
+        }
       }
+
+      return false;
     };
 
-    /**
-     * 열린 2가 되는 위치 찾기, 전달되는 포지션 기준 한 방향으로만 탐색
-     * @param position 시작 위치
-     * @param skip 스킵한 횟수
-     */
+    /** 열린 2 찾기 */
     const findOpenTwo = (position: Position): OpenTwo[] => {
       const result: ReturnType<typeof findOpenTwo> = [];
-
-      // 흑돌 연결된 2 위치 찾기
-      const find = (p: Position, d: Direction): [Position, Position] | undefined => {
-        let { x: cx, y: cy } = p;
-        const { dx, dy } = d;
-        let skipCount = 0;
-        let count = 0;
-        let startPosition = { x: 0, y: 0 };
-
-        while (Board.isValidStonePosition(createPosition(cx, cy))) {
-          const target = this.board.get(createPosition(cx, cy));
-
-          // 종료 조건
-          if (target === STONE.WHITE.POINT) break;
-          if (target === EMPTY && skipCount >= 3) break;
-
-          if (target === STONE.BLACK.POINT) {
-            if (count === 0) startPosition = createPosition(cx, cy);
-
-            count += 1;
-          }
-
-          if (count === 2) return [startPosition, createPosition(cx, cy)];
-          if (target === EMPTY) skipCount += 1;
-
-          cx += dx;
-          cy += dy;
-        }
-
-        return undefined;
-      };
-
-      for (let i = 0; i < DIRECTIONS.length; i += 1) {
-        const twoPosition = find(position, DIRECTIONS[i]);
-
-        if (twoPosition && checkOpenTwo(...twoPosition)) {
-          result.push(twoPosition);
-        }
-      }
-
-      return result;
-    };
-
-    /** 3*3 금수 위치 기준으로 열린 2 위치 구하기 */
-    const findOpenTwoForSamsamGeumsu = (position: Position): OpenTwo[] => {
-      const result: ReturnType<typeof findOpenTwoForSamsamGeumsu> = [];
       const cachedPosition = new Set();
 
-      const changePositionToString = (p1: Position, p2: Position) =>
-        `${p1.x}.${p1.y}-${p2.x}.${p2.y}`;
+      const positionToCacheData = (p1: Position, p2: Position) => `${p1.x}.${p1.y}-${p2.x}.${p2.y}`;
 
       const isPositionCached = (p1: Position, p2: Position) =>
-        cachedPosition.has(changePositionToString(p1, p2));
+        cachedPosition.has(positionToCacheData(p1, p2));
 
       const cachePosition = (p1: Position, p2: Position) => {
-        // 정방향, 역방향 캐싱
-        cachedPosition.add(changePositionToString(p1, p2));
-        cachedPosition.add(changePositionToString(p2, p1));
+        // 정방향, 역방향 위치 캐싱
+        cachedPosition.add(positionToCacheData(p1, p2));
+        cachedPosition.add(positionToCacheData(p2, p1));
       };
 
       // 흑돌 연결된 2 위치 찾기
@@ -200,10 +143,10 @@ class RenjuRule {
 
           if (count === 2) {
             const openTwoPosition: [Position, Position] = [startPosition, createPosition(cx, cy)];
-
-            // 캐싱된 경우 return
-            if (isPositionCached(...openTwoPosition)) return undefined;
-
+            // 캐싱된 경우 중복 처리 방지
+            if (isPositionCached(...openTwoPosition)) {
+              return undefined;
+            }
             // 캐싱되지 않은 경우 캐싱하고 위치 반환
             cachePosition(...openTwoPosition);
 
@@ -234,49 +177,33 @@ class RenjuRule {
     const findCanSamsamPosition = (openTwoPosition: OpenTwo): Position[] => {
       const result: Position[] = [];
       const [currentPosition, openTwoSpot] = openTwoPosition;
-      const distance = getDistance(currentPosition, openTwoSpot);
       const { x: tx, y: ty } = openTwoSpot;
       // 놓여진 돌 기준, 기존에 있는 돌 위치로 향하는 방향
       const { dx, dy } = Board.getDirection(currentPosition, openTwoSpot);
+      const distance = getDistance(currentPosition, openTwoSpot);
 
       const nextOpenTwo = createPosition(tx + dx, ty + dy);
       const nextCurrentStone = createPosition(x + -dx, y + -dy);
       const afterNextOpenTwo = createPosition(tx + dx * 2, ty + dy * 2);
       const afterNextCurrentStone = createPosition(x + -dx * 2, y + -dy * 2);
 
-      const isNextOpenTwoCanDropAndOpenThree =
-        this.board.canDropStone(nextOpenTwo) &&
-        this.checkOpenThree([currentPosition, openTwoSpot, nextOpenTwo]);
-      const isNextCurretStoneCanDropAndOpenThree =
-        this.board.canDropStone(nextCurrentStone) &&
-        this.checkOpenThree([openTwoSpot, currentPosition, nextCurrentStone]);
+      const checkOpenThreeWithPosition = (position: Position) => {
+        const isOpenThree =
+          this.board.canDropStone(position) &&
+          this.checkOpenThree([currentPosition, openTwoSpot, position]);
+
+        return isOpenThree;
+      };
 
       switch (distance) {
         // 붙은 2 (OO)
         case 1: {
           // 이은 3 위치가 열린 3인지 확인 후 3*3 가능 위치 추가
-          if (isNextOpenTwoCanDropAndOpenThree) {
-            result.push(nextOpenTwo);
-          }
-
-          if (isNextCurretStoneCanDropAndOpenThree) {
-            result.push(nextCurrentStone);
-          }
-
+          if (checkOpenThreeWithPosition(nextOpenTwo)) result.push(nextOpenTwo);
+          if (checkOpenThreeWithPosition(nextCurrentStone)) result.push(nextCurrentStone);
           // 띈 3 위치가 열린 3인지 확인 후 3*3 가능 위치 추가
-          if (
-            this.board.canDropStone(afterNextOpenTwo) &&
-            this.checkOpenThree([currentPosition, openTwoSpot, afterNextOpenTwo])
-          ) {
-            result.push(afterNextOpenTwo);
-          }
-
-          if (
-            this.board.canDropStone(afterNextCurrentStone) &&
-            this.checkOpenThree([openTwoSpot, currentPosition, afterNextCurrentStone])
-          ) {
-            result.push(afterNextCurrentStone);
-          }
+          if (checkOpenThreeWithPosition(afterNextOpenTwo)) result.push(afterNextOpenTwo);
+          if (checkOpenThreeWithPosition(afterNextCurrentStone)) result.push(afterNextCurrentStone);
 
           break;
         }
@@ -286,13 +213,8 @@ class RenjuRule {
           // 띈 2 사이 낀 위치 3*3 가능 위치 추가
           result.push(createPosition(x + dx, y + dy));
 
-          if (isNextOpenTwoCanDropAndOpenThree) {
-            result.push(nextOpenTwo);
-          }
-
-          if (isNextCurretStoneCanDropAndOpenThree) {
-            result.push(nextCurrentStone);
-          }
+          if (checkOpenThreeWithPosition(nextOpenTwo)) result.push(nextOpenTwo);
+          if (checkOpenThreeWithPosition(nextCurrentStone)) result.push(nextCurrentStone);
 
           break;
         }
@@ -320,7 +242,7 @@ class RenjuRule {
 
       for (let j = 0; j < canSamsamPositions.length; j += 1) {
         // 3*3 가능 자리 기준 open two 탐색
-        const potentialOpenTwos = findOpenTwoForSamsamGeumsu(canSamsamPositions[j]);
+        const potentialOpenTwos = findOpenTwo(canSamsamPositions[j]);
 
         // 3*3 가능 자리에 open two 2개 이상인 경우
         if (potentialOpenTwos.length >= 2) {
@@ -344,54 +266,55 @@ class RenjuRule {
 
   /** openTwo들로 spot이 3*3 금수가 가능한지 확인 */
   private checkCanSamsam(spot: Position, openTwos: OpenTwo[]) {
-    // 열린 2는 여러개가 될 수 있음
+    // 열린 2는 여러개 가능
     const filteredOpenTwos = openTwos.filter((openTwo) => this.checkOpenThree([spot, ...openTwo]));
 
-    // 교점에 open two가 2개 이상 존재해야 3*3 금수 가능
+    // 교점에 open two 2개 이상이면 3*3 금수 가능
     return filteredOpenTwos.length >= 2;
   }
 
   /** 해당 포지션에 돌 3개가 놓아졌을 때 open three인지 확인 */
-  private checkOpenThree(poistions: [Position, Position, Position]) {
-    poistions.sort((prev, curr) => prev.x - curr.x || prev.y - curr.y);
+  private checkOpenThree(positions: [Position, Position, Position]) {
+    positions.sort((prev, curr) => prev.x - curr.x || prev.y - curr.y);
 
-    // poistions[0]에서 poistions[2]으로 향하는 방향
-    const { dx, dy } = Board.getDirection(poistions[0], poistions[2]);
-    const distance = getDistance(poistions[0], poistions[2]);
+    // positions[0]에서 positions[2]으로 향하는 방향
+    const { dx, dy } = Board.getDirection(positions[0], positions[2]);
+    const distance = getDistance(positions[0], positions[2]);
     const bothSideEmpty =
-      this.board.canDropStone(createPosition(poistions[0].x + -dx, poistions[0].y + -dy)) &&
-      this.board.canDropStone(createPosition(poistions[2].x + dx, poistions[2].y + dy));
+      this.board.canDropStone(createPosition(positions[0].x + -dx, positions[0].y + -dy)) &&
+      this.board.canDropStone(createPosition(positions[2].x + dx, positions[2].y + dy));
+
+    if (!bothSideEmpty) return false;
 
     // 붙은 3 OOO
-    if (distance === 2) {
-      const isSequential = Board.isSequential(poistions);
-
-      return bothSideEmpty && isSequential;
-    }
+    if (distance === 2) return Board.isSequential(positions);
 
     // 띈 3 OVOO / OOVO
     if (distance === 3) {
-      // OVOO, OOVO 중 비어있는 셀 판별
-      const innerFirstCell = this.board.get(
-        createPosition(poistions[0].x + dx, poistions[0].y + dy),
-      );
-      const innerSecondCell = this.board.get(
-        createPosition(poistions[0].x + dx * 2, poistions[0].y + dy * 2),
-      );
+      let { x, y } = positions[0];
+      let skip = false;
 
-      if (innerFirstCell === EMPTY && innerSecondCell === EMPTY) {
-        const firstCell = this.board.get(poistions[0]);
-        const lastCell = this.board.get(poistions[2]);
+      for (let i = 0; i < positions.length - 1; i += 1) {
+        const currentPosition = createPosition(x, y);
 
-        return bothSideEmpty && firstCell === STONE.BLACK.POINT && lastCell === STONE.BLACK.POINT;
+        if (!Board.isValidStonePosition(currentPosition)) return false;
+        if (this.board.get(currentPosition) === STONE.WHITE.POINT) return false;
+
+        const next = positions[i + 1];
+
+        if (!(x + dx === next.x && y + dy === next.y)) {
+          if (skip || !this.board.canDropStone(createPosition(x + dx, y + dy))) return false;
+
+          skip = true;
+          x += dx;
+          y += dy;
+        }
+
+        x += dx;
+        y += dy;
       }
 
-      // 띈 3의 조건을 만족하는지 확인
-      const isGood =
-        (innerFirstCell === STONE.BLACK.POINT && innerSecondCell === EMPTY) ||
-        (innerFirstCell === EMPTY && innerSecondCell === STONE.BLACK.POINT);
-
-      return bothSideEmpty && isGood;
+      return true;
     }
 
     return false;
