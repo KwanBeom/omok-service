0c17ce2a8a0297ad987510957718919620308aa6
BK <3031011@naver.com>
2025-03-23 21:49:36 +0900

    feat(GameHandler): 오목 시간제한 서버측 로직 구현

--- server/socket/GameHandler.ts
+++ server/socket/GameHandler.ts
@@ -1,16 +1,13 @@
 import { Server, Socket } from 'socket.io';
-import {
-  EVENT_KEYS,
-  ClientPlayEvent,
-  ServerPlayEvent,
-  AssignStoneColorEvent,
-} from '../constants/events';
+import { EVENT_KEYS, ClientPlayEvent, ServerPlayEvent, GameStartEvent } from '../constants/events';
 import Omok from '../omok/core/Omok';
 import { BLACK, WHITE } from '../constants/player';
 import { STONE } from '../types/stone';
 import { getShuffledStones } from '../lib/utils';
 
 class GameHandler {
+  private TURN_TIME = 30;
+
   private players: Socket[];
 
   private io: Server;
@@ -18,6 +15,8 @@ class GameHandler {
   private omok: Omok;
 
   protected roomId = '';
+
+  private timerId: NodeJS.Timeout | null = null;
 
   constructor(io: Server) {
     this.io = io;
@@ -31,16 +30,14 @@ class GameHandler {
   }
 
   /** 게임 초기화, 플레이어에게 돌 색깔 할당 */
-  initGame(players: [Socket, Socket]) {
-    console.log('init');
+  initGame() {
     const colors: STONE[] = getShuffledStones();
-    this.players = players;
-
     // 플레이어들에게 돌 색 할당
     this.players.forEach((player, index) => {
-      const data: AssignStoneColorEvent['serverSend'] = { color: colors[index] };
-      player.emit(EVENT_KEYS.ASSIGN_STONE_COLOR, data);
+      const data: GameStartEvent['serverSend'] = { color: colors[index] };
+      player.emit(EVENT_KEYS.GAME_START, data);
     });
+    this.setTimer();
   }
 
   /** 게임 리셋 */
@@ -51,11 +48,10 @@ class GameHandler {
   /** 게임 진행 */
   playGame(data: ClientPlayEvent['clientSend']) {
     const { position, userColor } = data;
-    console.log(userColor, this.omok.getCurrentTurn());
     if (userColor !== this.omok.getCurrentTurn()) {
       throw new Error('올바르지 않은 사용자가 착수했습니다.');
     }
-
+    this.clearTimer();
     this.omok.play(position.x, position.y);
     const turn = this.omok.getCurrentTurn();
     const playData: ServerPlayEvent['serverSend'] = {
@@ -65,13 +61,43 @@ class GameHandler {
       turn,
     };
     this.broadcast(EVENT_KEYS.SERVER_PLAY, playData);
+    this.setTimer();
 
     return this.omok.checkWin();
+  }
+
+  /** 게임 준비완료 처리 */
+  readyGame(socket: Socket, ready: boolean) {
+    if (ready) {
+      const users = this.players.push(socket);
+      if (users === 2) {
+        this.initGame();
+      }
+    } else {
+      this.players = this.players.filter((player) => player.id !== socket.id);
+    }
   }
 
   /** 클라이언트들에게 메시지 전송 */
   private broadcast(key: string, data: unknown) {
     this.players.forEach((player) => player.emit(key, data));
+  }
+
+  /** 턴 시간제한 타이머 설정 */
+  private setTimer() {
+    this.timerId = setTimeout(() => {
+      this.broadcast(EVENT_KEYS.GAME_OVER, {
+        winner: this.omok.getCurrentTurn() === BLACK ? WHITE : BLACK,
+      });
+    }, 1000 * this.TURN_TIME);
+  }
+
+  /** 턴 시간제한 타이머 해제 */
+  private clearTimer() {
+    if (this.timerId) {
+      clearTimeout(this.timerId);
+      this.timerId = null; // 중복 실행 방지
+    }
   }
 }
 
